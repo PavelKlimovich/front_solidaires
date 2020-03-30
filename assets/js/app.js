320b538d1af7d609758a5ee9099bfa05d636c8cd
PavelKlimovich <pavelklimovich@hotmail.fr>
2020-03-30 20:17:08 +0200

    last

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,31 +21,84 @@ function check() {
     dd.classList.remove("col-6")
   }
  
-  // Get the modal
-  var modal = document.getElementById("myModal");
+  
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-  
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-  
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+//  // Get the modal
+//  var modal = document.getElementById("myModal1");
+
+//  // Get the button that opens the modal
+//  var btn = document.getElementById("myBtn1");
+ 
+//  // Get the <span> element that closes the modal
+//  var span = document.getElementById("close1");
+ 
+//  // When the user clicks on the button, open the modal
+//  btn.onclick = function() {
+//    modal.style.display = "block";
+//  }
+ 
+//  // When the user clicks on <span> (x), close the modal
+//  span.onclick = function() {
+//    modal.style.display = "none";
+//  }
+ 
+//  // When the user clicks anywhere outside of the modal, close it
+//  window.onclick = function(event) {
+//    if (event.target == modal) {
+//      modal.style.display = "none";
+//    }
+//  }
+
+
+ var modal = document.querySelector("#myModal");
+  var close = document.querySelector("#close");
+  var modal = document.querySelector("#myModal");
+
+
+var modalName = document.querySelector("#modalName");
+var modalFirstname =document.querySelector("#modalFirstname");
+var modalAbout =document.querySelector("#modalAbout");
+var modalLink =document.querySelector("#modalLink");
+
+
+function modalOpen(name,firstname,about,link) {
+  var modal = document.querySelector("#myModal");
+  var close = document.querySelector("#close");
+  var modal = document.querySelector("#myModal");
+
+
+var modalName = document.querySelector("#modalName");
+var modalFirstname =document.querySelector("#modalFirstname");
+var modalAbout =document.querySelector("#modalAbout");
+var modalLink =document.querySelector("#modalLink");
+
+console.log(name);
+
+
+modal.style.display = "block";
+modalName.innerText =  name ;
+modalFirstname.innerText = firstname ;
+modalAbout.innerText =  about ;
+modalLink.innerText =  link ;
+
+}
+
+close.onclick = function() {
+  var modal = document.querySelector("#myModal");
+  var close = document.querySelector("#close");
+  var modal = document.querySelector("#myModal");
+
+
+var modalName = document.querySelector("#modalName");
+var modalFirstname =document.querySelector("#modalFirstname");
+var modalAbout =document.querySelector("#modalAbout");
+var modalLink =document.querySelector("#modalLink");
+
     modal.style.display = "none";
-  }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
+}
+
+window.onclick = function(event) {
     if (event.target == modal) {
-      modal.style.display = "none";
-    }
+       modal.style.display = "none";
+     }
   }
-  
-
-
